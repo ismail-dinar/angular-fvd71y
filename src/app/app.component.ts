28bf41c3bc1661db3afbc5ef7da88fd84a8dc18d
ismail-dinar <sm41l.4796@gmail.com>
2022-04-30 12:27:36 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
 import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { WindowService, WindowRef, WindowCloseResult } from '@progress/kendo-angular-dialog';
-import { DialogContainerService } from '@progress/kendo-angular-dialog/dialog/dialog-container.service';
+import { DialogContainerService } from './dialog-container.service';
 
 @Component({
     selector: 'my-app',
     template: `
         <div class="example-wrapper">
-            <button kendoButton (click)="openWindow()">Open Window</button>
+            <button kendoButton (click)="openWindow('Window 1')">Open Window</button>
         </div>
 
         <div class="example-wrapper">
-            <button kendoButton (click)="openWindow()">Open Window</button>
+            <button kendoButton (click)="openWindow('Window 2')">Open Window</button>
         </div>
 
         <div class="example-wrapper">
-            <button kendoButton (click)="openWindow()">Open Window</button>
+            <button kendoButton (click)="openWindow('Window 3')">Open Window</button>
         </div>
         <div #container></div>
     `
@@ -24,17 +24,21 @@ export class AppComponent {
 
     constructor(private windowService: WindowService, private dialogContainerService: DialogContainerService) {}
 
-    public openWindow(): void {
-        const window: WindowRef = this.windowService.open({
-            title: 'My Window',
+    public openWindow(title: string): void {
+        const options = {
+            title,
             content: 'My Content!',
             width: 450,
             height: 200,
             appendTo: this.container,
-        });
+        }
+
+        this.dialogContainerService.open(options);
+        const window: WindowRef = this.windowService.open(options);
 
         window.result.subscribe((result) => {
             if (result instanceof WindowCloseResult) {
+                this.dialogContainerService.close(options);
                 console.log('Window was closed!');
             }
         });
--- src/app/dialog-container.service.ts
+++ src/app/dialog-container.service.ts
@@ -1,15 +1,29 @@
 import { Injectable, ViewContainerRef, ViewRef } from '@angular/core';
 import { WindowSettings } from '@progress/kendo-angular-dialog';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DialogContainerService {
   private static ViewContainerRefs: ViewContainerRef[] = [];
   private static ViewRefs: ViewRef[] = [];
-  constructor() { }
+  constructor() {}
 
   public open(options: WindowSettings): void {
     if (options && options.appendTo) {
+      const length = DialogContainerService.ViewContainerRefs.length;
+      if (length) {
+        const currentDialogContainer =
+          DialogContainerService.ViewContainerRefs[length - 1];
+        DialogContainerService.ViewRefs.push(currentDialogContainer.detach());
+      }
+      DialogContainerService.ViewContainerRefs.push(options.appendTo);
+    }
+  }
 
+  public close(options: WindowSettings): void {
+    if (options && options.appendTo) {
+      const previousContainer = DialogContainerService.ViewContainerRefs.pop();
+      previousContainer &&
+        previousContainer.insert(DialogContainerService.ViewRefs.pop());
     }
   }
 }
